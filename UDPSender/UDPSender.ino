3a636493db2fdf4d616ea93ad87bd38916a308d5
Dmitry K <dmitry@radiofid.ru>
2017-04-04 15:44:43 +0300

    added UIPEth sketches and lib

---
+++ UDPSender/UDPSender.ino
@@ -0,0 +1,28 @@
+#include <SPI.h>         // needed for Arduino versions later than 0018
+#include <UIPEthernet.h>
+
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+};
+IPAddress  localip(192, 168, 1, 177);
+IPAddress remoteip(192, 168, 1, 10);
+
+unsigned int remotePort = 8888;
+unsigned int localPort  = 8888;
+char  Reply[] = "usefull payload\n";
+
+EthernetUDP Udp;
+
+void setup() {
+  //void begin(const uint8_t* mac, IPAddress ip, IPAddress dns, IPAddress gateway, IPAddress subnet);
+  Ethernet.begin(mac, localip);
+  Serial.begin(9600);
+}
+
+void loop() {
+    sprintf(Reply, "%010lu\n", millis());
+    Udp.beginPacket(remoteip, remotePort);
+    Udp.write(Reply);
+    Udp.endPacket();
+}
+
