633963cce194d1fe7f700c7697a446c33c33c473
Dmitry Koroban <dmitry@radiofid.ru>
2019-01-28 15:41:32 +0300

    stm32-st7735-binclock: set time from terminal

--- stm32-st7735-binclock/stm32-st7735-binclock.ino
+++ stm32-st7735-binclock/stm32-st7735-binclock.ino
@@ -18,10 +18,12 @@
  
 Adafruit_ILI9341_STM tft = Adafruit_ILI9341_STM(TFT_CS, TFT_DC, TFT_RST); // Use hardware SPI
 
-uint8_t digits[] = {2,3,5,9,5,1}; //start value
+uint8_t digits[] = {1,1,5,9,5,1}; //start value
 uint8_t dig_max[] = {2,4,3,4,3,4}; //number of bits 
+#define SER_CNT_MAX 10
+char ser_in[SER_CNT_MAX];
+uint8_t ser_cnt = 0;
 int w, h;
-char init_time[] = __TIME__;
 
 void setup() {
   Serial.begin(115200);
@@ -29,17 +31,13 @@ void setup() {
   tft.setRotation(1);
   w = tft.width();
   h = tft.height();
-  //kinda stupid, but works...
-  digits[0] = init_time[0]-'0';
-  digits[1] = init_time[1]-'0';
-  digits[2] = init_time[3]-'0';
-  digits[3] = init_time[4]-'0';
-  digits[4] = init_time[6]-'0';
-  digits[5] = init_time[7]-'0';
 }
 
 
 void loop(void) {
+  //serialEvent does not seem to work in STM32, got to do it manually
+  if(Serial.available()) {serialEvent();}
+
   //time in text
   tft.fillScreen(ILI9341_BLACK);
   tft.setCursor(TEXT_X, TEXT_Y);
@@ -58,12 +56,15 @@ void loop(void) {
   for(int cnt=0; cnt < 6 ; cnt++) {
     digit(digits[cnt], dig_max[cnt], cnt*DIG_STEP+X_SHIFT+(cnt/2*COLS_GAP));
   }
+
+  //FIXME: this is very inaccurate, got to find more precise way
   delay(1000);
+  
   digits[5]++;
   if(digits[5] > 9) { digits[5]=0; digits[4]++; } //1 sec
   if(digits[4] > 5) { digits[4]=0; digits[3]++; } //10 sec
   if(digits[3] > 9) { digits[3]=0; digits[2]++; } //1 min
-  if(digits[2] > 5) { digits[5]=0; digits[1]++; } //10 min
+  if(digits[2] > 5) { digits[2]=0; digits[1]++; } //10 min
   if(digits[0] < 2) {
     if(digits[1] > 9) { digits[1]=0; digits[0]++; } //1 hour
   } else {
@@ -86,4 +87,28 @@ void rect(int x, int y, int i, int color, int filled) {
   }
   //tft.fillCircle(x, y, radius, color);
   //tft.drawCircle(x, y, radius, color);
+}
+
+
+void serialEvent(){
+  while(Serial.available()) {
+    ser_in[ser_cnt] = Serial.read();
+    Serial.print(ser_in[ser_cnt]);
+    if(ser_cnt < SER_CNT_MAX) ser_cnt++;
+  }
+  if(ser_cnt > 5){
+    ser_cnt = 0;
+    for(int i = 0; i < 6; i++){
+        //check if all received bytes are valid digits
+        if(ser_in[i] < '0' || ser_in[i] > '9'){
+          Serial.println("\nInvalid input");
+          return;
+        }
+    }
+    //apply time
+    for(int i = 0; i < 6; i++){
+      digits[i] = ser_in[i]-'0';
+    }
+    Serial.println("\nTime set!");
+  }
 }
